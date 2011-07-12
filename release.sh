6a9450ed29b416efff924ac48ab08ab3a6d7ee1f
Quentin Stievenart <quentin.stievenart@gmail.com>
2011-07-12 19:09:45 +0200

    small changes for the howto

--- release.sh
+++ release.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 sbcl --load build.lisp --eval '(build:build)' --eval '(sb-ext:quit)'
+
+emacs --batch --visit howto.org --funcall org-export-as-html-batch
+if [ -f howto.html ]
+then
+  mv howto.html release/
+fi
